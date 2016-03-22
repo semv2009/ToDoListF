0acbf2e65d810da5cf11af65df358dca7c1d372b
semv2009 <niksemv@gmail.com>
2016-03-22 22:29:16 +0600

    Update CreateNewTaskViewController.swift
    Изменение экрана создания нового задания

--- ToDoList/CreateNewTaskViewController.swift
+++ ToDoList/CreateNewTaskViewController.swift
@@ -8,12 +8,20 @@
 
 import UIKit
 
-class CreateNewTaskViewController: UIViewController,UITextFieldDelegate {
-
+class CreateNewTaskViewController: UIViewController,UITextFieldDelegate,UIPickerViewDelegate{
+    
+    var task:Task?
+    let importanceArray = Importance.allValue
+    
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var dateTextField: UITextField!
     @IBOutlet weak var importanceTextField: UITextField!
-   
+    
+    var importancePiker: UIPickerView{
+        didSet{
+            importancePiker.delegate = self
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,6 +31,20 @@ class CreateNewTaskViewController: UIViewController,UITextFieldDelegate {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "unwideTask" {
+            //if nameLabel.text?.characters.count != 0{
+                let name = nameLabel.text!
+           // }
+            //if descriptionLabel.text?.characters.count != 0{
+                let description = descriptionLabel.text!
+            //}
+            let importance = importanceArray[importancePiker.selectedRowInComponent(0)]
+            
+            print(importance)
+            task = Task(name: name, descriptionTask: description, date: dataPiker.date, importance: Importance(rawValue: importance) ?? .Usually)
+        }
     }
     
     // MARK: - TextFiend Delegate
@@ -36,6 +58,9 @@ class CreateNewTaskViewController: UIViewController,UITextFieldDelegate {
             let dataPiker = UIDatePicker()
             textField.inputView = dataPiker
             dataPiker.addTarget(self, action: "datePikerChanged:", forControlEvents: .ValueChanged)
+        }else if textField == importanceTextField{
+            textField.inputView = importancePiker
+            importancePiker.addTarget(self, action: "importancePikerChanged:", forControlEvents: .ValueChanged)
         }
     }
     
@@ -43,6 +68,23 @@ class CreateNewTaskViewController: UIViewController,UITextFieldDelegate {
         let formatter = NSDateFormatter()
         formatter.dateStyle = .FullStyle
         dateTextField.text = formatter.stringFromDate(sender.date)
+    }
+    
+    func importancePikerChanged(sender:UIPickerView){
+        importanceTextField.text = importanceArray[sender.selectedRowInComponent(0)]
+    }
+    
+    // MARK: - Piker View
+    func numberOfComponentsInPickerView(pickerView: UIPickerView!) -> Int{
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView!, numberOfRowsInComponent component: Int) -> Int{
+        return importanceArray.count
+    }
+    
+    func pickerView(pickerView: UIPickerView!, titleForRow row: Int, forComponent component: Int) -> String!{
+        return importanceArray[row]
     }
     /*
     // MARK: - Navigation
